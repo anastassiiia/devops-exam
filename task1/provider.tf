fe923b1fea6eeb775ced2fb745069752edd37a10
anastassiiia <anastasiia.chuikova.pp.2022@lpnu.ua>
2026-04-07 04:27:37 +0300

    Add Terraform provider configuration for DigitalOcean

---
+++ task1/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.36.0"
+    }
+  }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+    region                      = "us-east-1"
+    bucket                      = "chuikova-terraform-remote-state"
+    key                         = "task1/terraform.tfstate"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true 
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token # або через ENV
+}
